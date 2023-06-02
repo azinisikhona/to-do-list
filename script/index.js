72507f6a45d9c72df8b68a051b8a8cb627123fb1
azinisikhona <azizsiyalana10@gmail.com>
2023-06-02 16:39:13 +0200

    ayikasebenzi le-wei

--- script/index.js
+++ script/index.js
@@ -1,13 +1,21 @@
 let Items = [];
 
 function add(){
-    let input = document.getElementById('input').value;
-    if(input == ""){
+    let input = document.getElementById('input');
+    let items = input.value;
+    input.value = "";
+    if(items == ""){
         alert("add a valid item");
     }else{
-        Items.push(input);
-        console.log(Items);
+        Items.push(items);
+        return;
+        // console.log(Items);
     }
+
+    let itemsList = document.getElementById('dis');
+    let newItem = document.createElement("div");
+    newItem.className = "item";
+    itemsList.appendChild(newItem);
 
     document.getElementById('dis').innerHTML = Items;
 }
